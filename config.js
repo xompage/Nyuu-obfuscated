5bf9013c9ea58494f244f842f58bddbefda5fe0b
animetosho <animetosho@users.noreply.github.com>
2015-08-04 21:51:26 +1000

    Add basic, very rough NNTP client

--- config.js
+++ config.js
@@ -2,8 +2,10 @@ module.exports = {
 	
 	// usenet server
 	server: { // connection settings
-		host: 'news.example.com',
-		port: 119,
+		connect: {
+			host: 'news.example.com',
+			port: 119,
+		},
 		secure: false, // set to 'true' to use SSL
 		user: null,
 		password: null,
@@ -11,6 +13,7 @@ module.exports = {
 		timeout: 60000, // in ms
 		connTimeout: 30000, // in ms
 		reconnectDelay: 5000, // in ms
+		connectRetries: 3,
 		// TODO: reconnect, max retries etc
 	},
 	connections: 3, // number of connections
@@ -19,6 +22,8 @@ module.exports = {
 		// same as 'server' above; missing fields are copied from there
 	},
 	headerCheckConnections: 0, // probably not much of a reason to go above 1
+	headerCheckDelays: [40, 20], // further retries will use the last number specified
+	headerCheckRetries: 3,
 	// TODO: check delay, max tries, multiple servers?
 	
 	articleSize: 768000,
