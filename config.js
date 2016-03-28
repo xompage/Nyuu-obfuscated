b2d855a44620a60d12f164464f861750b77d2798
animetosho <animetosho@users.noreply.github.com>
2016-03-28 19:46:12 +1000

    Remove unneeded check-group option Message-ID STATing doesn't need a group selected

--- config.js
+++ config.js
@@ -36,7 +36,6 @@ module.exports = {
 		delay: 40*1000, // (in ms) initial delay for performing check
 		recheckDelay: 20*1000, // (in ms) delay retries by this amount of time
 		tries: 0, // number of check attempts; should be 0 if not performing post checks
-		group: '', // which group to check in; if left blank, will auto determine from posting headers
 		ulConnReuse: false, // use uploading connections for post checks; only works if checking the same server as the one being uploaded to
 		postRetries: 1, // maximum number of post retry attempts after a post check failure; set to 0 to never retry posting
 		ignoreFailure: false, // what to do once all post retry attempts have been exhausted; either error and halt the process (true) or ignore, print a warning, and assume the last post attempt succeeded (false)
