d43b2f229640ec288eef426dc5b9bf5d956a7fec
animetosho <animetosho@users.noreply.github.com>
2020-12-25 01:43:30 +1000

    Display times on timing test failures

--- test/_testlib.js
+++ test/_testlib.js
@@ -19,6 +19,7 @@ module.exports = {
 	assertTimeWithin: function(start, from, to) {
 		var taken = Date.now() - start;
 		// give 5ms leeway in timing
-		assert(taken >= from-5 && (!to || taken < to+5));
+		if(taken < from-5) assert.equal(taken, from);
+		if(to && taken >= to+5) assert.equal(taken, to);
 	},
 };
